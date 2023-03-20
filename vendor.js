74c9b82bf64741b9c433854f35d3658e441aa59f
Ju-Tsu <example@gmail.com>
2023-03-20 14:41:48 +0400

    Final Gorshok

--- vendor.js
+++ vendor.js
@@ -1,8 +1,24 @@
-var btn = document.querySelector('.cta_button');
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("cta_button");
+var span = document.getElementsByClassName("close")[0];
+btn.addEventListener('click', function() {
+  modal.style.display = "block";
+});
+// When the user clicks on <span> (x), close the modal
+span.addEventListener('click', function() {
+  modal.style.display = "none";
+});
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener('click', function(event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
 function getNumberOfDays() {
   let date = '2013-07-19T00:00:00.000Z';
-  let currentDate = Date.parse(new Date());
-  let days = (currentDate - Date.parse(date)) / 86400000;
-  console.log(Math.round(days));
-  alert("Горшок не пьет уже " + Math.round(days) + " дней");
+  let currentDate = new Date();
+  return days = (currentDate - Date.parse(date)) / 86400000;
 }
+document.getElementById('info').innerHTML = 'Горшок не пьет уже ' + Math.round(getNumberOfDays()) + ' дней';
+
